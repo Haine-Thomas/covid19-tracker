213a1257ce26b6de9ecb80cd2e4f61bacd072d88
Mushi2216 <thomahaine@gmail.com>
2020-10-13 15:34:44 +0200

    first cards and components and fetching datas to put them in cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
-function App() {
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
+
+import { fetchData } from './api/index';
+
+class App extends React.Component {
+  state = {
+    data: {},
+  }
+  async componentDidMount(){
+    const fetchedData = await fetchData();
+  this.setState({ data: fetchedData })
+  }
+ render() {
+   const { data } = this.state;
+   
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className={styles.container}>
+      <Cards  data={ data } />
+      <Chart />
+      <CountryPicker />
     </div>
-  );
+    );
+  }
 }
+  
 
 export default App;
