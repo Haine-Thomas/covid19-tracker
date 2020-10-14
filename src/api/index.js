524f77d2b7097a6fa9e2796962faaf57269fdf2c
Mushi2216 <thomahaine@gmail.com>
2020-10-14 16:13:03 +0200

    picker of country names ok

--- src/api/index.js
+++ src/api/index.js
@@ -23,7 +23,19 @@ export const fetchDailyData = async () => {
     try {
       const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
       const reverseData = data.reverse();
-      return reverseData.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+      const modifiedData = reverseData.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+      return modifiedData;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  export const fetchCountries = async () => {
+    try {
+      const { data: { countries } } = await axios.get(`${url}/countries`);
+  
+      const fetchedCountries = countries.map((country) => country.name);
+      return fetchedCountries;
     } catch (error) {
       return error;
     }
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+import { fetchCountries } from '../../api';
+
+import styles from './CountryPicker.module.css';
+
 
 const CountryPicker = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+          setCountries(await fetchCountries());
+        };
+    
+        fetchAPI();
+      }, [setCountries]);
+      console.log(countries);
     return (
-        <h1>CountryPicker</h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                 <option value="global">USA</option>
+                 {countries.map((country, i)=> <option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     )
 }
 
